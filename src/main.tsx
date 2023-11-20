4b00b8142d767634ab4465a237c5b3bfc5b3d3b0
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2023-11-20 21:41:46 +0100

    Add react-oidc-context
    Implement oidc/oauth2 flow using
    https://github.com/authts/oidc-client-ts and
    https://github.com/authts/react-oidc-context.
    Add init_env.sh to inject container env variables at
    run time.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { AuthProvider, type AuthProviderProps } from "react-oidc-context";
 import App from './App.tsx'
 import './index.css'
 
+// Add `env` namespace to window
+interface EnvInterface {
+  IAM_AUTHORITY: string;
+  IAM_CLIENT_ID: string;
+  IAM_CLIENT_SECRET?: string;
+  IAM_SCOPE: string;
+  IAM_AUDIENCE: string;
+}
+
+declare global {
+  interface Window { env: EnvInterface }
+}
+
+const oidcConfig: AuthProviderProps = {
+  authority: window.env.IAM_AUTHORITY,
+  client_id: window.env.IAM_CLIENT_ID,
+  client_secret: window.env.IAM_CLIENT_SECRET,
+  scope: window.env.IAM_SCOPE,
+  redirect_uri: window.location.origin
+};
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <AuthProvider {...oidcConfig}>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
 )

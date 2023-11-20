9e8b7bee481e7804f063cba5c56a42209845da62
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2023-11-20 21:41:46 +0100

    Add react-oidc-context
    Implement oidc/oauth2 flow using
    https://github.com/authts/oidc-client-ts and
    https://github.com/authts/react-oidc-context.
    Add init_env.sh to inject container env variables at
    run time.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,59 @@
 import { useState } from 'react'
+import { useAuth } from "react-oidc-context";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const auth = useAuth();
+  const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+  switch (auth.activeNavigator) {
+    case "signinSilent":
+      return <div>Signing you in...</div>;
+    case "signoutRedirect":
+      return <div>Signing you out...</div>;
+  }
+
+  if (auth.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (auth.error) {
+    return <div>Oops... {auth.error.message}</div>;
+  }
+
+  if (auth.isAuthenticated) {
+    return (
+      <>
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Hello, {auth.user?.profile.name}!</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+        <div>
+          <button onClick={() => void auth.removeUser()}>Log out</button>
+        </div>
+      </>
+    )
+  }
+
+  return <button onClick={() => void auth.signinRedirect()}>Log in</button>;
 }
 
 export default App

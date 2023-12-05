516a8e4d2635c641624c9d044f95d09f5c5d8853
Jacopo Gasparetto <jacopo.gasparetto@gmail.com>
2023-12-05 11:56:51 +0100

    feat: Go back to lower case Logo

--- src/App.tsx
+++ src/App.tsx
@@ -44,7 +44,7 @@ function App() {
             <img src={viteLogo} className="logo" alt="Vite logo" />
           </a>
           <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React Logo" />
+            <img src={reactLogo} className="logo react" alt="React logo" />
           </a>
         </div>
         <h1>Hello, {auth.user?.profile.name}!</h1>

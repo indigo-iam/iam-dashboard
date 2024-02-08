5d70317d32635b8b36f23fb26377068b49778702
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-02-08 14:21:05 +0000

    feat: add tests

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+export default {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.(ts|tsx)?$": [
+      "ts-jest",
+      { diagnostics: { ignoreCodes: ["TS151001"] } },
+    ],
+  },
+};

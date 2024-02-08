0ba3f89c385b763ca5f47634b4ce504a08ce7332
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-02-08 14:48:09 +0100

    refactor: Change NGINX proxy pass routes

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/iam-dashboard", 
   server: {
     port: 3000,
     host: true,
     hmr: {
       path: "/ws",
-      clientPort: 3000
+      clientPort: 8080 // this MUST match the NGINX's listening port
     }
   }
 })

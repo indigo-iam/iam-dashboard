415fa9fd5641dcfd20dccb5d54fa8aad1c829cf5
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-02-08 23:32:48 +0100

    feat: export components

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { Button, type ButtonProps } from "./Button";
+export { Drawer, type DrawerProps } from "./Drawer";
+export { LogoHeader, type LogoHeaderProps } from "./LogoHeader";
+export { Page, type PageProps } from "./Page";

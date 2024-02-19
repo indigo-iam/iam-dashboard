2bbfea89af3e9e6d9e76e0c36fc7f2e08d4dd83c
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-02-19 13:44:32 +0000

    feat: Show groups with delete button

--- src/pages/Home/components/GroupsCard.tsx
+++ src/pages/Home/components/GroupsCard.tsx
@@ -1,4 +1,6 @@
+import { Button } from "../../../components";
 import { useIam } from "../../../services/IAM";
+import { UserPlusIcon } from "@heroicons/react/16/solid";
 
 export const GroupsCard = (): JSX.Element => {
   const iam = useIam();
@@ -7,15 +9,47 @@ export const GroupsCard = (): JSX.Element => {
       return null;
     }
     const { groups } = iam.user;
+    if (!groups) {
+      return <p>No groups found</p>;
+    }
+
+    const Row = (props: { title: string }) => {
+      return (
+        <tr>
+          <td>
+            <b>{props.title}</b>
+          </td>
+          <td className="d-flex flex-row-reverse">
+            <Button color="danger">Delete</Button>
+          </td>
+        </tr>
+      );
+    };
+
     return (
       <div>
-        <p>{groups ? groups : "No groups found"}</p>
+        <table>
+          <tbody>
+            {groups.map(group => {
+              return <Row key={group.display} title={group.display} />;
+            })}
+            <div className="container mt-4">
+              <div className="row">
+                <div className="col">
+                  <Button icon={<UserPlusIcon />} color="success">
+                    Add Group
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </tbody>
+        </table>
       </div>
     );
   };
   return (
     <div className="infn-card">
-      <div className="infn-title">Groups</div>
+      <div className="infn-title text-center mb-2">Groups</div>
       <Groups />
     </div>
   );

10ba469d5918a04a3fbf45dce00a31c36321198d
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-02-19 13:08:12 +0000

    feat: Add Edit Details and Change Password Buttons

--- src/pages/Home/components/UserCard.tsx
+++ src/pages/Home/components/UserCard.tsx
@@ -1,5 +1,7 @@
+import { Button } from "../../../components";
 import { IamUser } from "../../../services/IAM";
 import { useIam } from "../../../services/IAM";
+import { PencilIcon, KeyIcon } from "@heroicons/react/16/solid";
 
 export const UserCard = (): JSX.Element => {
   const iam = useIam();
@@ -40,13 +42,30 @@ export const UserCard = (): JSX.Element => {
     };
 
     return (
-      <table className="w-100">
-        <tbody>
-          {data.map(row => (
-            <Row key={row.key} title={row.key} value={row.value} />
-          ))}
-        </tbody>
-      </table>
+      <div>
+        <p>{user.id}</p>
+        <table className="w-100">
+          <tbody>
+            {data.map(row => (
+              <Row key={row.key} title={row.key} value={row.value} />
+            ))}
+          </tbody>
+        </table>
+        <div className="container justify-content-center mt-4">
+          <div className="row align-items-center">
+            <div className="col">
+              <Button className="mx-auto" icon={<PencilIcon />}>
+                Edit Details
+              </Button>
+            </div>
+            <div className="col">
+              <Button className="mx-auto" icon={<KeyIcon />} color="success">
+                Change Password
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   };
   return (

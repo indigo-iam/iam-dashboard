ba82cbc4f9ccb3df3b412eac066e0ed47d9d7893
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-02-19 14:47:49 +0000

    feat: show OIDC and SAML Linked accounts

--- src/pages/Home/components/LinkedAccountsCard.tsx
+++ src/pages/Home/components/LinkedAccountsCard.tsx
@@ -1,4 +1,7 @@
+import { Button } from "../../../components";
 import { useIam } from "../../../services/IAM";
+import { OidcId, SamlId } from "../../../services/IAM/IamUser";
+import { XCircleIcon } from "@heroicons/react/16/solid";
 
 export const LinkedAccountsCard = (): JSX.Element => {
   const LinkedAccounts = (): JSX.Element => {
@@ -6,30 +9,78 @@ export const LinkedAccountsCard = (): JSX.Element => {
     if (!iam.user) {
       return <></>;
     }
-    const { samlIds } = iam.user["urn:indigo-dc:scim:schemas:IndigoUser"];
+
+    const OidcIdView = (props: { id: OidcId }) => {
+      return (
+        <div className="container border-bottom p-2">
+          <div className="row">
+            <div className="col">
+              <b>Issuer:</b> {props.id.issuer}
+              <br />
+              <b>Subject:</b>
+              {props.id.subject}
+            </div>
+            <div className="col d-flex flex-row-reverse">
+              <Button
+                color="danger"
+                className="my-auto"
+                icon={<XCircleIcon />}
+                style={{ height: "32px" }}
+              >
+                Unlink
+              </Button>
+            </div>
+          </div>
+        </div>
+      );
+    };
+
+    const SamlIdView = (props: { id: SamlId }) => {
+      return (
+        <div className="container border-bottom p-2">
+          <div className="row">
+            <div className="col">
+              {props.id.idpId}
+              <br />
+              {props.id.attributeId}
+              <br />
+              {props.id.userId}
+            </div>
+            <div className="col d-flex flex-row-reverse">
+              <Button
+                color="danger"
+                className="my-auto"
+                icon={<XCircleIcon />}
+                style={{ height: "32px" }}
+              >
+                Unlink
+              </Button>
+            </div>
+          </div>
+        </div>
+      );
+    };
+
+    const { oidcIds, samlIds } =
+      iam.user["urn:indigo-dc:scim:schemas:IndigoUser"];
     return (
       <div>
-        {samlIds.map(el => {
-          return (
-            <ul key={el.idpId}>
-              <li>
-                <b>User Id</b> {el.userId}
-              </li>
-              <li>
-                <b>Idp Id</b> {el.idpId}
-              </li>
-              <li>
-                <b>Attribute Id</b> {el.attributeId}
-              </li>
-            </ul>
-          );
-        })}
+        <b>OpenID Connect</b>
+        <br />
+        {oidcIds.map(oidcId => (
+          <OidcIdView key={oidcId.subject} id={oidcId} />
+        ))}
+        <b>SAML</b>
+        <br />
+        {samlIds.map((samlId, i) => (
+          <SamlIdView key={`saml-id-${i}`} id={samlId} />
+        ))}
       </div>
     );
   };
   return (
     <div className="infn-card">
-      <div className="infn-title">Linked Accounts</div>
+      <div className="infn-title text-center">Linked Accounts</div>
       <LinkedAccounts />
     </div>
   );

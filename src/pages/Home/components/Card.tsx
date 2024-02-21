d975a9a173a3e86286fb30079910f5b48f576005
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-02-21 11:17:39 +0000

    refactor: change Card font to infn-subtitle

--- src/pages/Home/components/Card.tsx
+++ src/pages/Home/components/Card.tsx
@@ -11,7 +11,7 @@ export const Card = (props: CardProps) => {
   const { title, children, footer } = props;
   return (
     <div className="infn-card">
-      <div className="infn-title text-center mb-2">{title}</div>
+      <div className="infn-subtitle text-center mb-2">{title}</div>
       {children}
       <div>{footer}</div>
     </div>

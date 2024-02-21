44a0a113405c80608dfaafc9e5d435f5e3f79c89
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-02-21 22:59:57 +0000

    feat: Add Card title style

--- src/pages/Home/components/Card.tsx
+++ src/pages/Home/components/Card.tsx
@@ -11,7 +11,7 @@ export const Card = (props: CardProps) => {
   const { title, children, footer } = props;
   return (
     <div className="infn-card">
-      <div className="infn-subtitle text-center mb">{title}</div>
+      <div className="infn-card-title">{title}</div>
       <div className="infn-card-body">{children}</div>
       <div className="infn-card-footer">{footer}</div>
     </div>

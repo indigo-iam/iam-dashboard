8e8f29318f5b0789db85e3ab0cb7042de122f91e
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-02-21 21:54:26 +0000

    refactor: remove unused code

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -39,39 +39,4 @@ export const Modal = (props: ModalProps) => {
       </div>
     </div>
   );
-
-  return (
-    <div
-      className={`modal fade ${show ? "show" : "hide"}`}
-      tabIndex={-1}
-      role="dialog"
-      style={{
-        display: "block",
-        transition: "all 300ms",
-        visibility: show ? "visible" : "hidden",
-        // background: "#00000090",
-      }}
-      aria-modal={true}
-    >
-      <div className="modal-dialog" role="document">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h5 className="modal-title">{title}</h5>
-            <button
-              type="button"
-              className="infn-btn-close"
-              aria-label="Close"
-              onClick={close}
-            >
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div className="modal-body">
-            <p>{body}</p>
-          </div>
-          <div className="modal-footer">{footer}</div>
-        </div>
-      </div>
-    </div>
-  );
 };

52ceded1aa2055098ab9235d7e898f3561bc9379
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-03-04 15:50:51 +0000

    refactor: Extract inner components from their parents

--- src/pages/Home/components/UserCard/ChangePasswordModal.tsx
+++ src/pages/Home/components/UserCard/ChangePasswordModal.tsx
@@ -5,58 +5,59 @@ import {
   XMarkIcon,
 } from "@heroicons/react/24/solid";
 
+const Body = () => {
+  return (
+    <Form>
+      <Input type="password" id="current-password" title="Current Password" />
+      <Input type="password" id="new-password" title="New Password" />
+      <Input type="password" id="repeat-password" title="Repeat Password" />
+    </Form>
+  );
+};
+
+const Footer = (props: { onClose: () => void }) => {
+  const { onClose } = props;
+  return (
+    <div className="d-flex justify-content-end p-2">
+      <div className="row">
+        <div className="col p-1">
+          <Button
+            className="my-auto"
+            color="primary"
+            icon={<ArrowUpTrayIcon />}
+          >
+            Update Password
+          </Button>
+        </div>
+        <div className="col p-1">
+          <Button
+            className="my-auto"
+            color="warning"
+            icon={<ArrowUturnLeftIcon />}
+          >
+            Reset
+          </Button>
+        </div>
+        <div className="col p-1">
+          <Button
+            className="my-auto"
+            color="danger"
+            onClick={onClose}
+            icon={<XMarkIcon />}
+          >
+            Cancel
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export const ChangePasswordModal = (props: {
   show: boolean;
   onClose: () => void;
 }) => {
   const { show, onClose } = props;
-
-  const Body = () => {
-    return (
-      <Form>
-        <Input type="password" id="current-password" title="Current Password" />
-        <Input type="password" id="new-password" title="New Password" />
-        <Input type="password" id="repeat-password" title="Repeat Password" />
-      </Form>
-    );
-  };
-
-  const Footer = () => {
-    return (
-      <div className="d-flex justify-content-end p-2">
-        <div className="row">
-          <div className="col p-1">
-            <Button
-              className="my-auto"
-              color="primary"
-              icon={<ArrowUpTrayIcon />}
-            >
-              Update Password
-            </Button>
-          </div>
-          <div className="col p-1">
-            <Button
-              className="my-auto"
-              color="warning"
-              icon={<ArrowUturnLeftIcon />}
-            >
-              Reset
-            </Button>
-          </div>
-          <div className="col p-1">
-            <Button
-              className="my-auto"
-              color="danger"
-              onClick={onClose}
-              icon={<XMarkIcon />}
-            >
-              Cancel
-            </Button>
-          </div>
-        </div>
-      </div>
-    );
-  };
 
   return (
     <Modal
@@ -64,7 +65,7 @@ export const ChangePasswordModal = (props: {
       onClose={onClose}
       title="Change Password"
       body={<Body />}
-      footer={<Footer />}
+      footer={<Footer onClose={onClose} />}
     />
   );
 };

9261a0e76367e25a00a8c02f4f8dbff0fdfe0473
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-03-04 15:38:48 +0000

    refactor: Extract inner components from their parents

--- src/pages/Home/components/GroupRequestsCard/index.tsx
+++ src/pages/Home/components/GroupRequestsCard/index.tsx
@@ -2,68 +2,68 @@ import { useIam, IamGroupRequestResource } from "@services/IAM";
 import { XCircleIcon } from "@heroicons/react/16/solid";
 import { Button } from "@components";
 import { Card } from "../Card";
+import { IamGroupRequests } from "src/services/IAM/models/GroupRequest";
+
+const Row = (props: { title: string; value: string }) => {
+  const { title, value } = props;
+  return (
+    <div className="row">
+      <div className="col-2">
+        <b>{title}</b>
+      </div>
+      <div className="col">{value}</div>
+    </div>
+  );
+};
+
+const GroupRequest = (props: { resource: IamGroupRequestResource }) => {
+  const { resource } = props;
+  const { userFullName, username, uuid, groupName, groupUuid } = resource;
+
+  return (
+    <div className="container border-bottom p-2 mt-2">
+      <div className="row">
+        <div className="col">
+          <Row title="Username" value={userFullName ?? username} />
+          <Row title="User ID" value={uuid} />
+          <Row title="Group" value={groupName} />
+          <Row title="Group ID" value={groupUuid} />
+        </div>
+        <div className="col-auto d-flex flex-row-reverse">
+          <Button
+            color="danger"
+            className="my-auto"
+            icon={<XCircleIcon />}
+            small={true}
+          >
+            Unlink
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const GroupRequests = (props: { groupRequests: IamGroupRequests }) => {
+  const { groupRequests } = props;
+  return (
+    <div>
+      {groupRequests.Resources.map((resource, i) => {
+        return <GroupRequest key={`group-request-${i}`} resource={resource} />;
+      })}
+    </div>
+  );
+};
 
 export const GroupRequestsCard = (): JSX.Element => {
   const { groupRequests } = useIam();
+  if (!groupRequests) {
+    return <Card title="Group Requests">No Request Found</Card>;
+  }
 
-  const GroupRequests = () => {
-    if (!groupRequests) {
-      return "No requests found";
-    }
-
-    const GroupRequest = (props: { resource: IamGroupRequestResource }) => {
-      const { resource } = props;
-      const { userFullName, username, uuid, groupName, groupUuid } = resource;
-
-      const Row = (props: { title: string; value: string }) => {
-        const { title, value } = props;
-        return (
-          <div className="row">
-            <div className="col-2">
-              <b>{title}</b>
-            </div>
-            <div className="col">{value}</div>
-          </div>
-        );
-      };
-
-      return (
-        <div className="container border-bottom p-2 mt-2">
-          <div className="row">
-            <div className="col">
-              <Row title="Username" value={userFullName ?? username} />
-              <Row title="User ID" value={uuid} />
-              <Row title="Group" value={groupName} />
-              <Row title="Group ID" value={groupUuid} />
-            </div>
-            <div className="col-auto d-flex flex-row-reverse">
-              <Button
-                color="danger"
-                className="my-auto"
-                icon={<XCircleIcon />}
-                small={true}
-              >
-                Unlink
-              </Button>
-            </div>
-          </div>
-        </div>
-      );
-    };
-
-    return (
-      <div>
-        {groupRequests.Resources.map((resource, i) => {
-          return (
-            <GroupRequest key={`group-request-${i}`} resource={resource} />
-          );
-        })}
-      </div>
-    );
-  };
   return (
     <Card title="Group Requests">
-      <GroupRequests />
+      <GroupRequests groupRequests={groupRequests} />
     </Card>
   );
 };

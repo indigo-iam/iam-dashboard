8116ae740bd9320c623277115829cd8453fa8f67
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-03-04 15:52:17 +0000

    refactor: Extract inner components from their parents

--- src/pages/Home/components/UserCard/EditDetailsModal.tsx
+++ src/pages/Home/components/UserCard/EditDetailsModal.tsx
@@ -5,67 +5,68 @@ import {
 } from "@heroicons/react/24/outline";
 import { XMarkIcon } from "@heroicons/react/24/solid";
 
+const Body = () => {
+  return (
+    <Form>
+      <Input type="search" id="name" title="Name" />
+      <Input type="search" id="surname" title="Surname" />
+      <Input type="email" id="email" title="Email" />
+      <Input type="search" id="username" title="Username" />
+      <Input type="search" id="picture" title="Picture" />
+    </Form>
+  );
+};
+
+const Footer = (props: { onClose: () => void }) => {
+  const { onClose } = props;
+  return (
+    <div className="d-flex justify-content-end p-2">
+      <div className="row">
+        <div className="col p-1">
+          <Button
+            className="my-auto"
+            color="primary"
+            icon={<ArrowUpTrayIcon />}
+          >
+            Update Password
+          </Button>
+        </div>
+        <div className="col p-1">
+          <Button
+            className="my-auto"
+            color="warning"
+            icon={<ArrowUturnLeftIcon />}
+          >
+            Reset
+          </Button>
+        </div>
+        <div className="col p-1">
+          <Button
+            className="my-auto"
+            color="danger"
+            onClick={onClose}
+            icon={<XMarkIcon />}
+          >
+            Cancel
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export const EditDetailsModal = (props: {
   show: boolean;
   onClose: () => void;
 }) => {
   const { show, onClose } = props;
-  const Body = () => {
-    return (
-      <Form>
-        <Input type="search" id="name" title="Name" />
-        <Input type="search" id="surname" title="Surname" />
-        <Input type="email" id="email" title="Email" />
-        <Input type="search" id="username" title="Username" />
-        <Input type="search" id="picture" title="Picture" />
-      </Form>
-    );
-  };
-
-  const Footer = () => {
-    return (
-      <div className="d-flex justify-content-end p-2">
-        <div className="row">
-          <div className="col p-1">
-            <Button
-              className="my-auto"
-              color="primary"
-              icon={<ArrowUpTrayIcon />}
-            >
-              Update Password
-            </Button>
-          </div>
-          <div className="col p-1">
-            <Button
-              className="my-auto"
-              color="warning"
-              icon={<ArrowUturnLeftIcon />}
-            >
-              Reset
-            </Button>
-          </div>
-          <div className="col p-1">
-            <Button
-              className="my-auto"
-              color="danger"
-              onClick={onClose}
-              icon={<XMarkIcon />}
-            >
-              Cancel
-            </Button>
-          </div>
-        </div>
-      </div>
-    );
-  };
-
   return (
     <Modal
       show={show}
       onClose={onClose}
       title="Edit Details"
       body={<Body />}
-      footer={<Footer />}
+      footer={<Footer onClose={onClose} />}
     />
   );
 };

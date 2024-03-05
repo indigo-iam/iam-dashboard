dc278473f64857dfb30991d88e1854b93b842f5d
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-03-05 23:14:12 +0000

    InputProps extends HTMLInputElement

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,39 +1,17 @@
-export interface InputProps {
-  id: string;
-  title: string;
-  type: React.HTMLInputTypeAttribute;
-  value?: string;
-  placeholder?: string;
-  onChange?: (newValue?: string) => void;
-}
+interface InputProps extends React.HTMLProps<HTMLInputElement> {}
 
 export const Input = (props: InputProps) => {
-  const { id, title, type, value, placeholder, onChange } = props;
+  const { id, title, type } = props;
   const className =
     type === "search" || type === "email" || type === "password"
       ? "infn-input-search"
       : "";
-
-  const onChangeCallback = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (onChange) {
-      onChange(e.currentTarget.value);
-    }
-  };
-
   return (
     <div className="row p-1">
       <label className="col col-lg-6 col-xl-5 col-xxl-4 my-auto" htmlFor={id}>
         {title}
       </label>
-      <input
-        className={`col-1 col-lg ${className}`}
-        type={type}
-        name={id}
-        value={value}
-        placeholder={placeholder}
-        onChange={onChangeCallback}
-        id={id}
-      />
+      <input className={`col-1 col-lg ${className}`} {...props} />
     </div>
   );
 };

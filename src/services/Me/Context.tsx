8302c908354ba6e55df8cdd4faceff8a55ce7e57
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-03-06 15:44:27 +0000

    Enforce MeContextProps

--- src/services/Me/Context.tsx
+++ src/services/Me/Context.tsx
@@ -1,6 +1,10 @@
 import { createContext } from "react";
 import { State } from "./reducer";
+import { GroupRequests, Me } from "@models/Me";
 
-export interface MeContextProps extends State {}
+export interface MeContextProps extends State {
+  me: Me;
+  groupRequest: GroupRequests;
+}
 
 export const MeContext = createContext<MeContextProps | undefined>(undefined);

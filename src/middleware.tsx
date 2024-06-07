dfe8cde59283da1e07c823682ef79f2c6a126983
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-06-07 17:21:15 +0200

    Add comment on the form submit redirect hack

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -24,6 +24,10 @@ export default auth(req => {
     req.nextUrl.pathname !== "/signin"
   ) {
     console.log("access token expired, redirect to /signout");
+    // This is an hack to make redirects on form submits work, based on
+    // https://github.com/vercel/next.js/blob/0cf0d43a48e04820d081de59176cbd75dd4bf193/packages/next/src/client/components/router-reducer/reducers/server-action-reducer.ts#L82
+    // Since it is not in the official documentation, consider if this logic
+    // must be updated in the future.
     if (req.method === "POST") {
       const response = new NextResponse();
       response.headers.set("x-action-redirect", "/signout");

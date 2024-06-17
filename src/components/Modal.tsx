d00b7405932dd5f7cca006ee5f96505aaadb59ee
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-06-17 16:12:37 +0200

    Add footer top border

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -24,7 +24,7 @@ export const ModalBody = (props: { children?: ReactNode }) => {
 export const ModalFooter = (props: { children?: ReactNode }) => {
   const { children } = props;
   return (
-    <div className="bottom-0 flex min-h-8 justify-end space-x-2 pt-4">
+    <div className="bottom-0 flex min-h-8 justify-end space-x-2 border-t-2 pt-4">
       {children}
     </div>
   );

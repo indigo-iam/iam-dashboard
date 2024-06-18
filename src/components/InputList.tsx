abe1a06610e057eea577a398e412ed8ed551278a
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-06-18 17:28:17 +0200

    Add InputList component

---
+++ src/components/InputList.tsx
@@ -0,0 +1,54 @@
+import { useRef, useState } from "react";
+import { PlusIcon, XMarkIcon } from "@heroicons/react/16/solid";
+import Button from "@/components/Button";
+
+interface InputListProps extends React.HTMLProps<HTMLInputElement> {
+  items: string[];
+  addItem: (value: string) => void;
+  removeItem: (index: number) => void;
+}
+
+export default function InputList(props: Readonly<InputListProps>) {
+  const { items, addItem, removeItem, ...inputProps } = props;
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [value, setValue] = useState("");
+
+  const listItems = props.items.map((item, index) => (
+    <li key={item} className="mt-1 flex flex-row items-center gap-2 text-sm">
+      <button
+        type="button"
+        onClick={() => props.removeItem(index)}
+        className="w-5 rounded bg-secondary-100 hover:bg-danger hover:text-secondary"
+      >
+        <XMarkIcon />
+      </button>
+      {item}
+    </li>
+  ));
+
+  const handleClick = () => {
+    addItem(value);
+    setValue("");
+    if (inputRef.current) {
+      inputRef.current.value = "";
+    }
+  };
+
+  return (
+    <div className="flex flex-col">
+      <div className="flex flex-row items-center gap-2">
+        <input
+          ref={inputRef}
+          onChange={event => setValue(event.target.value)}
+          className="w-full border p-2"
+          {...inputProps}
+        />
+        <Button type="button" icon={<PlusIcon />} onClick={handleClick}>
+          Add
+        </Button>
+      </div>
+      <ul className="mt-2">{listItems}</ul>
+    </div>
+  );
+}

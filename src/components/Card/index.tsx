020778e49fee9e0d5ba082ca7c00d0735c870631
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-06-18 10:07:34 +0200

    Set default card font size to small

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -9,11 +9,11 @@ export interface CardProps {
 export default function Card(props: Readonly<CardProps>) {
   const { title, children, footer } = props;
   return (
-    <div className="rounded-xl p-8 shadow-xl transition duration-300 hover:scale-105">
+    <div className="rounded-xl p-8 text-sm shadow-xl transition duration-300 hover:scale-105">
       <div className="border-b-2 pb-2 text-center text-lg font-bold uppercase">
         {title}
       </div>
-      <div className="px-4 py-8 border-b-2">{children}</div>
+      <div className="border-b-2 px-4 py-8">{children}</div>
       <div className="mt-8">{footer}</div>
     </div>
   );

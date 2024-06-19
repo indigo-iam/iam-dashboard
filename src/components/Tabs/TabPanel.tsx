9770515fe974bb18426c4e911feaea61f57eaa30
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-06-19 13:01:58 +0200

    Allow for external classNames

--- src/components/Tabs/TabPanel.tsx
+++ src/components/Tabs/TabPanel.tsx
@@ -2,12 +2,13 @@ import { TabPanel as HeadlessUITabPanel } from "@headlessui/react";
 
 type TapPanelProps = {
   children: React.ReactNode;
+  className?: string;
 };
 
 export default function TabPanel(props: Readonly<TapPanelProps>) {
-  const { children } = props;
+  const { children, className } = props;
   return (
-    <HeadlessUITabPanel className="rounded-lg p-4 shadow">
+    <HeadlessUITabPanel className={"rounded-lg p-4 shadow " + className}>
       {children}
     </HeadlessUITabPanel>
   );

45b3789935c481d1ed735f4d52e5f4518342afb0
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-06-19 12:00:59 +0200

    Simplify Client Secret tab structure and set defaultValue from API

--- src/app/(pages)/clients/[client]/components/tabs/Credentials/ClientSecret.tsx
+++
@@ -1,9 +0,0 @@
-import Button from "@/components/Button";
-
-export default function ClientSecret() {
-  return (
-    <div className="max-w-54">
-      <Button>Regenerate Client Secret</Button>
-    </div>
-  );
-}
--- src/app/(pages)/clients/[client]/components/tabs/Credentials/RegistrationAccessToken.tsx
+++
@@ -1,9 +0,0 @@
-import Button from "@/components/Button";
-
-export default function RegistrationAccessToken() {
-  return (
-    <div className="max-w-54">
-      <Button>Regenerate Registration Access Token</Button>
-    </div>
-  );
-}

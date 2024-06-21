27e4f066c69af3935139390556ba5035fa86087e
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-06-21 19:51:42 +0200

    Cast numbers and boolean in formDataToJSON

--- src/utils/misc/index.ts
+++ src/utils/misc/index.ts
@@ -1,11 +1,21 @@
 export const formDataToJSON = <T>(formData: FormData) => {
-  const json: { [key: string]: string | File | boolean | string[] | File[] } =
-    {};
+  const json: {
+    [key: string]: string | File | boolean | string[] | File[] | number;
+  } = {};
   for (const [key, value] of formData.entries()) {
     if (formData.getAll(key).length > 1) {
       json[key] = formData.getAll(key) as string[];
     } else if (value) {
-      json[key] = value === "on" ? true : value;
+      const n = parseInt(value.toString());
+      if (n) {
+        json[key] = Number(n);
+      } else if (value === "on") {
+        json[key] = true;
+      } else if (value === "false") {
+        json[key] = false;
+      } else {
+        json[key] = value;
+      }
     }
   }
   return json as T;

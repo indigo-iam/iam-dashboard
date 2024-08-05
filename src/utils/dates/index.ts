e301483f2d3e6049235f07d6e9a4eaf53038a8d1
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-08-05 12:33:18 +0200

    Compute date diffs as days/months/years

--- src/utils/dates/index.ts
+++ src/utils/dates/index.ts
@@ -1,7 +1,19 @@
 export function dateToHuman(date: Date) {
-  const now = new Date();
-  if (now.getTime() - date.getTime() < 86400000) {
-    return "Today";
+  const now = Date.now();
+  const delta = now - date.getTime();
+
+  const formatter = new Intl.RelativeTimeFormat("en");
+
+  const days = Math.ceil(delta / 86400000);
+  if (days < 31) {
+    return formatter.format(-days, "day");
   }
-  return "Long time ago...";
+
+  const months = Math.floor(delta / 2678400000);
+  if (months < 12) {
+    return formatter.format(-months, "month");
+  }
+
+  const years = Math.floor(delta / 32140800000);
+  return formatter.format(-years, "year");
 }

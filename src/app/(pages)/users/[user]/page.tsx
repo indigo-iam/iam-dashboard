570a3e243a413c2df9684f0187628438e4f92b8b
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-08-06 15:09:58 +0200

    Add Certificates section to user page

--- src/app/(pages)/users/[user]/page.tsx
+++ src/app/(pages)/users/[user]/page.tsx
@@ -4,6 +4,7 @@ import Panel from "@/components/Panel";
 import UserInfo from "@/components/User/Info";
 import Section from "@/components/Section";
 import { GroupRequests, Groups, LinkedAccounts } from "@/components/User";
+import { Certificates } from "@/components/User/Certificates";
 
 type UserPageProps = {
   params: { user: string };
@@ -32,6 +33,9 @@ export default async function UserPage(props: Readonly<UserPageProps>) {
         </Section>
         <Section title="Linked Accounts">
           <LinkedAccounts user={user} />
+        </Section>
+        <Section title="X509 Certificates">
+          <Certificates user={user} />
         </Section>
       </Panel>
     </Page>
---
+++ src/components/User/Certificates/index.tsx
@@ -0,0 +1,38 @@
+import { Certificate, ScimUser } from "@/models/scim";
+
+const CertificateView = (props: { cert: Certificate }) => {
+  const { cert } = props;
+  return (
+    <div className="border-bottom pb-2">
+      <b>Subject</b> {cert.subjectDn} <br />
+      <b>Issuer</b> {cert.issuerDn} <br />
+      <b>Last Modified</b> {cert.lastModified}
+    </div>
+  );
+};
+
+type CertificateProps = {
+  user: ScimUser;
+};
+export const Certificates = async (props: Readonly<CertificateProps>) => {
+  const { user } = props;
+  let certificates: Certificate[] | undefined;
+
+  if (user["urn:indigo-dc:scim:schemas:IndigoUser"]) {
+    certificates = user["urn:indigo-dc:scim:schemas:IndigoUser"].certificates;
+  }
+
+  if (certificates && certificates?.length == 0) {
+    return <p>"No certificates found"</p>;
+  }
+
+  return (
+    <>
+      {certificates?.map(cert => {
+        return (
+          <CertificateView key={cert.subjectDn + cert.issuerDn} cert={cert} />
+        );
+      })}
+    </>
+  );
+};

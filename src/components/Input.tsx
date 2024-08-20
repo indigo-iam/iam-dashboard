561b2906766f217089bb893f277d5eb6c39deba7
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-08-20 18:57:33 +0200

    Wrap Input component into div for custom class

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,18 +5,19 @@ export interface InputProps extends React.HTMLProps<HTMLInputElement> {
 }
 
 export default function Input(props: Readonly<InputProps>) {
-  const { id, title } = props;
-  const { errorMessage, ...inputProps } = props;
+  const { id, title, className, errorMessage, ...inputProps } = props;
   return (
-    <div className="flex flex-col">
-      <Label htmlFor={id} title={title} />
-      <input
-        className="rounded-full bg-secondary-100 px-4 py-2 disabled:bg-secondary-300 placeholder:disabled:text-secondary-600"
-        {...inputProps}
-      />
-      {errorMessage ? (
-        <small className="text-danger">{errorMessage}</small>
-      ) : null}
+    <div className={className}>
+      <div className="flex flex-col">
+        <Label htmlFor={id} title={title} />
+        <input
+          className="rounded-full bg-secondary-100 px-4 py-2 disabled:bg-secondary-300 placeholder:disabled:text-secondary-600"
+          {...inputProps}
+        />
+        {errorMessage ? (
+          <small className="text-danger">{errorMessage}</small>
+        ) : null}
+      </div>
     </div>
   );
 }

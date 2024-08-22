ad0711e6293a0b28e4222a79a1ebc17fac2b4b1b
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-08-22 17:24:00 +0200

    Set Panel max width

--- src/components/Layout/Panel.tsx
+++ src/components/Layout/Panel.tsx
@@ -5,6 +5,8 @@ type PanelProps = {
 export default function Panel(props: Readonly<PanelProps>) {
   const { children } = props;
   return (
-    <div className="space-y-6 rounded-xl p-4 text-sm shadow">{children}</div>
+    <div className="m-auto max-w-screen-xl space-y-6 rounded-xl p-4 text-sm shadow">
+      {children}
+    </div>
   );
 }

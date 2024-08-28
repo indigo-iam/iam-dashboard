1d6181e846008180c9a0080b8f47bcbc03af6b12
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-08-28 10:04:26 +0200

    Adjust Dropdown button style

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -2,14 +2,18 @@ import {
   Select as HeadlessSelect,
   SelectProps as HeadlessSelectProps,
 } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/16/solid";
 
 interface SelectProps extends HeadlessSelectProps {}
 
 export default function Select(props: Readonly<SelectProps>) {
   return (
-    <HeadlessSelect
-      className="rounded-md border bg-secondary p-2 shadow-md"
-      {...props}
-    />
+    <div className="relative w-48">
+      <HeadlessSelect
+        className="block w-full appearance-none rounded-md border bg-secondary p-2 shadow-md"
+        {...props}
+      />
+      <ChevronDownIcon className="group absolute right-2.5 top-2.5 size-4" />
+    </div>
   );
 }

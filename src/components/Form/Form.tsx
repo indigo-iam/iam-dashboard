49cb248feac89add9258c7af40234e967bbe6375
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-08-29 12:49:04 +0200

    Simplify Form component

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -3,6 +3,5 @@ import React from "react";
 export interface FormProps extends React.HTMLProps<HTMLFormElement> {}
 
 export default function Form(props: FormProps) {
-  const { children, ...formProps } = props;
-  return <form {...formProps}>{children}</form>;
+  return <form {...props} />;
 }

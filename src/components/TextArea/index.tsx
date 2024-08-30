ceadff0e6419dd9d6a4dd15a70dbdec832e44160
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-08-30 14:44:05 +0200

    Adjust TextArea style

--- src/components/TextArea/index.tsx
+++ src/components/TextArea/index.tsx
@@ -1,15 +1,11 @@
 interface TextAreaProps extends React.HTMLProps<HTMLTextAreaElement> {}
 
 export default function TextArea(props: Readonly<TextAreaProps>) {
-  const { id, name, required } = props;
-
   return (
     <div className="flex flex-col">
       <textarea
-        className="rounded-xl bg-secondary-100 px-4 py-2 disabled:bg-secondary-300 placeholder:disabled:text-secondary-600"
-        id={id}
-        name={name}
-        required={required}
+        className="rounded-xl border bg-secondary px-4 py-2 disabled:bg-secondary-300 placeholder:disabled:text-secondary-600"
+        {...props}
       />
     </div>
   );

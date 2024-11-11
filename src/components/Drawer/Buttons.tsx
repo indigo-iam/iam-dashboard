6bba760947d6dd8779a201f673f69cc18ba17aeb
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-11-11 19:36:39 +0100

    Clear dead code

--- src/components/Drawer/Buttons.tsx
+++ src/components/Drawer/Buttons.tsx
@@ -1,4 +1,3 @@
-import { signOut } from "next-auth/react";
 import { ArrowRightEndOnRectangleIcon } from "@heroicons/react/24/solid";
 import { DrawerButton } from "./DrawerButton";
 import Notifications from "@/components/Notifications";
@@ -6,9 +5,6 @@ import { auth } from "@/auth";
 
 export const DrawerButtons = async () => {
   const session = await auth();
-  const logout = async () => {
-    await signOut();
-  };
   return (
     <div className="flex justify-around p-4">
       {session?.is_admin ? <Notifications /> : null}

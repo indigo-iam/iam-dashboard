6b3750a9ce9c6ad7da13ca616d99874647a11f04
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-11-12 15:47:13 +0100

    Adjust inputs padding

--- src/components/Form/FormSection.tsx
+++ src/components/Form/FormSection.tsx
@@ -7,8 +7,8 @@ export type FormSectionProps = {
 export default function FormSection(props: Readonly<FormSectionProps>) {
   const { htmlFor, title, children } = props;
   return (
-    <div className="mt-2 flex flex-col text-sm">
-      <label htmlFor={htmlFor} className="text-base font-bold py-2">
+    <div className="flex flex-col text-sm gap-2">
+      <label htmlFor={htmlFor} className="text-base font-bold">
         {title}
       </label>
       {children}
--- src/components/Inputs/Input.tsx
+++ src/components/Inputs/Input.tsx
@@ -12,7 +12,7 @@ export function Input(props: Readonly<InputProps>) {
   return (
     <div className={className}>
       <HeadlessInput
-        className="w-full rounded-lg border px-4 py-2 disabled:bg-secondary-300 placeholder:disabled:text-secondary-600 dark:bg-primary"
+        className="w-full rounded-lg border px-4 py-1 disabled:bg-secondary-300 placeholder:disabled:text-secondary-600 dark:bg-primary"
         {...inputProps}
       />
     </div>

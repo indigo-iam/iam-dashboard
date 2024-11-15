bb64257b79834d47af519ce1df0496149973ddd7
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-11-15 17:23:17 +0100

    Remove old import

--- src/components/Buttons/index.ts
+++ src/components/Buttons/index.ts
@@ -1,4 +1,3 @@
 export { default as Button } from "./Button";
 export { default as AddButton } from "./AddButton";
 export { default as DeleteButton } from "./DeleteButton";
-export { default as UnlinkMemberButton } from "./UnlinkMemberButton";

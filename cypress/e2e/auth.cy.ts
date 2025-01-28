c55c74e82f76cfeb1e31fb63e6215c887d88cd60
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2025-01-28 19:39:39 +0100

    First cypress e2e login test

---
+++ cypress/e2e/auth.cy.ts
@@ -0,0 +1,33 @@
+const login = (username: string, password: string) => {
+  // https://docs.cypress.io/api/commands/session#Cross-domain-sessions
+  cy.visit("https://iam-dev.cloud.cnaf.infn.it/login"); // do not remove it
+
+  cy.session(
+    username,
+    () => {
+      cy.visit("/signin").then(() => {
+        cy.get("#username").type(username);
+        cy.get("#password").type(password, {
+          log: false,
+        });
+        cy.get("#login-submit").click();
+      });
+    },
+    {
+      validate() {
+        cy.url().should("match", /.+\/users\/me$/);
+      },
+    }
+  );
+};
+
+describe("template spec", () => {
+  beforeEach(() => {
+    login(Cypress.env("IAM_ADMIN_USER"), Cypress.env("IAM_ADMIN_PASSWD"));
+  });
+
+  it("test 1", () => {
+    cy.log("heyy!!!");
+    cy.visit("/");
+  });
+});

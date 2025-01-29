5048e614c52ceb74705021a6861caf2fe5d1b44d
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2025-01-29 16:53:23 +0100

    Change oauth login procedure and re-enable websecurity

--- cypress/e2e/auth.cy.ts
+++ cypress/e2e/auth.cy.ts
@@ -1,17 +1,23 @@
 export const login = (username: string, password: string) => {
   // https://docs.cypress.io/api/commands/session#Cross-domain-sessions
-  cy.visit("https://iam-dev.cloud.cnaf.infn.it/login"); // do not remove it
+  cy.visit("http://localhost:8080"); // do not remove it
 
   cy.session(
     username,
     () => {
-      cy.visit("/signin").then(() => {
-        cy.get("#username").type(username);
-        cy.get("#password").type(password, {
-          log: false,
-        });
-        cy.get("#login-submit").click();
-      });
+      cy.visit("/signin");
+      cy.origin(
+        "https://iam-dev.cloud.cnaf.infn.it",
+        { args: { username, password } },
+        ({ username, password }) => {
+          cy.get("#username").type(username);
+          cy.get("#password").type(password, {
+            log: false,
+          });
+          cy.get("#login-submit").click();
+        }
+      );
+      cy.visit("http://localhost:8080");
     },
     {
       validate() {

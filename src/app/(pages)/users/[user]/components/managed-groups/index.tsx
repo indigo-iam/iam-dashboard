0394109b69b3d2d5d24b60429e66fced0cee6a42
Ivan <ivan03ds@gmail.com>
2025-01-31 11:04:34 +0100

    Refactor managed groups in user page for consistency

--- src/app/(pages)/users/[user]/components/managed-groups/index.tsx
+++ src/app/(pages)/users/[user]/components/managed-groups/index.tsx
@@ -12,9 +12,11 @@ export default async function ManagedGroups(
 ) {
   const { user } = props;
   const { managedGroups } = await fetchManagedGroups(user.id);
+
   if (managedGroups.length === 0) {
     return null;
   }
+  
   return (
     <Section title="Managed Groups">
       <ManagedGroupsTable managedGroups={managedGroups} />
--- src/app/(pages)/users/[user]/components/managed-groups/table/index.tsx
+++ src/app/(pages)/users/[user]/components/managed-groups/table/index.tsx
@@ -1,6 +1,22 @@
 import Link from "@/components/link";
 import { ManagedGroup } from "@/models/groups";
 
+type RowProps = {
+  group: ManagedGroup;
+};
+
+function Row(props: Readonly<RowProps>) {
+  const { group } = props;
+
+  return (
+    <tr className="tbl-tr">
+      <td className="tbl-td grow">
+        <Link href={`/groups/${group.id}`}>{group.name}</Link>
+      </td>
+    </tr>
+  );
+};
+
 type ManagedGroupsTableProps = {
   managedGroups: ManagedGroup[];
 };
@@ -9,17 +25,14 @@ export default function ManagedGroupsTable(
   props: Readonly<ManagedGroupsTableProps>
 ) {
   const { managedGroups } = props;
+
   return (
     <table className="w-full table-auto rounded-lg">
       <tbody>
-        {managedGroups.map(group => (
-          <tr key={group.id} className="tbl-tr">
-            <td className="tbl-td">
-              <Link href={`/groups/${group.id}`}>{group.name}</Link>
-            </td>
-          </tr>
-        ))}
+        {managedGroups.map(group => {
+          return <Row key={group.id} group={group} />;
+        })}
       </tbody>
     </table>
   );
-}
+};

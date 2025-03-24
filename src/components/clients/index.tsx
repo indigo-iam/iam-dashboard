759b392de33855aaea793a9b2daab615e2217b50
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2025-03-24 11:05:30 +0100

    Restyle clients table

--- src/components/clients/index.tsx
+++ src/components/clients/index.tsx
@@ -1,24 +1,52 @@
 import { Client } from "@/models/client";
-import Link from "@/components/link";
+import Link from "next/link";
 import ClientOptions from "./options";
+import { dateToHuman } from "@/utils/dates";
+import { Status } from "../badges";
 
 type RowProps = {
   client: Client;
 };
 
+function sortScopes(scope: String) {
+  const scopes = scope.split(" ");
+  scopes.sort();
+  return scopes.join(" ");
+}
+
 function Row(props: Readonly<RowProps>) {
   const { client } = props;
-  const { client_id, client_name } = client;
+  const { client_id, client_name, scope } = client;
+  const scopes = scope ? sortScopes(scope) : undefined;
+  const createdAt = client.created_at
+    ? dateToHuman(new Date(client.created_at))
+    : "N/A";
   return (
-    <tr className="tbl-hover tbl-tr">
-      <td className="tbl-td">{client_name}</td>
-      <td className="tbl-td">
-        <Link href={`/clients/${client_id}`}>{client_id}</Link>
-      </td>
-      <td className="tbl-td w-1/12 text-center">
-        <ClientOptions client={client} />
-      </td>
-    </tr>
+    <li className="flex flex-row rounded border-b p-2 last:border-b-0 hover:border-transparent hover:bg-neutral-200">
+      <Link
+        className="my-auto flex grow flex-col gap-1"
+        href={`/clients/${client_id}`}
+      >
+        <div className="flex grow flex-col hover:underline">
+          <div className="font-bold">{client_name}</div>
+          <small>{client_id}</small>
+        </div>
+        <div title={scopes} className="iam-text-light line-clamp-1 p-1 text-xs">
+          {scopes}
+        </div>
+      </Link>
+      <div className="flex flex-col">
+        <div className="my-auto flex flex-row">
+          <div className="flex flex-col items-end px-2">
+            <Status active={client.active} />
+            <small className="iam-text-light min-w-48 text-right">
+              Created {createdAt}
+            </small>
+          </div>
+          <ClientOptions client={client} />
+        </div>
+      </div>
+    </li>
   );
 }
 
@@ -29,19 +57,10 @@ type ClientsTableProps = {
 export default function ClientsTable(props: Readonly<ClientsTableProps>) {
   const { clients } = props;
   return (
-    <table className="w-full table-auto">
-      <thead>
-        <tr className="tbl-hover">
-          <th className="tbl-th text-left">Name</th>
-          <th className="tbl-th text-left">Client Id</th>
-          <th className="tbl-th text-center" />
-        </tr>
-      </thead>
-      <tbody>
-        {clients.map(client => (
-          <Row key={client.client_id} client={client} />
-        ))}
-      </tbody>
-    </table>
+    <ul className="w-full">
+      {clients.map(client => (
+        <Row key={client.client_id} client={client} />
+      ))}
+    </ul>
   );
 }

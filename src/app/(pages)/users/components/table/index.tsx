8222d42989db71b245b05002689c015f0f545cc4
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2025-03-24 17:04:02 +0100

    Improve response design for mobile

--- src/app/(pages)/users/components/table/index.tsx
+++ src/app/(pages)/users/components/table/index.tsx
@@ -14,22 +14,28 @@ function Row(props: Readonly<RowProps>) {
     ? dateToHuman(new Date(user.meta.created))
     : "N/A";
   return (
-    <div className="flex flex-row border-b p-2 hover:rounded-md hover:bg-neutral-200 has-[+:hover]:border-transparent">
-      <Link
-        className="flex grow flex-col font-bold hover:underline"
-        href={`/users/${user.id}`}
-      >
-        {user.name?.formatted}
-        <small className="iam-text-light">{user.emails?.[0].value}</small>
-      </Link>
-      <div className="flex flex-row">
-        <div className="hidden flex-col items-end px-2 sm:flex">
-          <Status active={user.active ?? false} />
-          <small className="iam-text-light">Created {created}</small>
+    <li className="flex flex-row border-b p-2 hover:rounded-md hover:bg-neutral-200 has-[+:hover]:border-transparent">
+      <div className="flex grow flex-col">
+        <div className="flex flex-col sm:flex-row gap-2">
+          <Link
+            className="flex grow flex-col font-bold hover:underline"
+            href={`/users/${user.id}`}
+          >
+            {user.name?.formatted}
+            <small className="iam-text-light">{user.emails?.[0].value}</small>
+          </Link>
+          <div className="my-auto flex grow flex-col">
+            <div className="inline-flex gap-2 sm:flex-col sm:items-end sm:gap-0 sm:px-2">
+              <Status active={user.active ?? false} />
+              <small className="iam-text-light">Created {created}</small>
+            </div>
+          </div>
         </div>
+      </div>
+      <div className="flex flex-col">
         <UserOptions user={user} />
       </div>
-    </div>
+    </li>
   );
 }
 
@@ -40,10 +46,10 @@ type UsersTableProps = {
 export default function UsersTable(props: Readonly<UsersTableProps>) {
   const { users } = props;
   return (
-    <div className="w-full">
+    <ul className="w-full">
       {users.map(user => (
         <Row key={user.id} user={user} />
       ))}
-    </div>
+    </ul>
   );
 }
--- src/components/clients/index.tsx
+++ src/components/clients/index.tsx
@@ -23,28 +23,35 @@ function Row(props: Readonly<RowProps>) {
     : "N/A";
   return (
     <li className="flex flex-row rounded border-b p-2 last:border-b-0 hover:border-transparent hover:bg-neutral-200 has-[+:hover]:border-transparent">
-      <Link
-        className="my-auto flex grow flex-col gap-1"
-        href={`/clients/${client_id}`}
-      >
-        <div className="flex grow flex-col hover:underline">
-          <div className="font-bold">{client_name}</div>
-          <small>{client_id}</small>
-        </div>
-        <div title={scopes} className="iam-text-light line-clamp-1 p-1 text-xs">
-          {scopes}
-        </div>
-      </Link>
-      <div className="flex flex-col">
-        <div className="my-auto flex flex-row">
-          <div className="hidden flex-col items-end px-2 sm:flex">
-            <Status active={client.active} />
-            <small className="iam-text-light min-w-48 text-right">
-              Created {createdAt}
-            </small>
+      <div className="flex grow flex-col">
+        <div className="flex flex-col sm:flex-row">
+          <Link
+            className="my-auto flex flex-col gap-1"
+            href={`/clients/${client_id}`}
+          >
+            <div className="flex flex-col hover:underline">
+              <div className="font-bold">{client_name}</div>
+              <small>{client_id}</small>
+            </div>
+            <div
+              title={scopes}
+              className="iam-text-light line-clamp-1 p-1 text-xs"
+            >
+              {scopes}
+            </div>
+          </Link>
+          <div className="my-auto flex grow flex-col">
+            <div className="inline-flex gap-2 sm:flex-col sm:items-end sm:gap-0 sm:px-2">
+              <Status active={client.active} />
+              <small className="iam-text-light min-w-48 sm:text-right">
+                Created {createdAt}
+              </small>
+            </div>
           </div>
-          <ClientOptions client={client} />
         </div>
+      </div>
+      <div className="flex flex-col">
+        <ClientOptions client={client} />
       </div>
     </li>
   );

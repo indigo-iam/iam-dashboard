6dcdddbc0ad3b97761dc9d09c7906745e9393ca5
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2025-05-16 22:19:30 +0200

    Color danger zone with danger red

--- src/app/(pages)/users/[user]/components/general/danger-zone.tsx
+++ src/app/(pages)/users/[user]/components/general/danger-zone.tsx
@@ -14,7 +14,7 @@ export function DangerZone(props: Readonly<DangerZoneProps>) {
   return (
     <>
       <div className="col-span-full text-sm font-light sm:col-span-2">
-        <div className="text-light">Danger zone</div>
+        <div className="text-danger py-1">Danger zone</div>
         <div className="text-extralight">
           A disabled user cannot login and their Access Tokens and Refresh
           Tokens are immediately revoked.

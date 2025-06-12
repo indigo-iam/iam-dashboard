ea2c27fc725cbe82019519f80255fa485461f77c
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2025-06-12 16:32:46 +0200

    Adjust description font size

--- src/components/form/description.tsx
+++ src/components/form/description.tsx
@@ -11,7 +11,7 @@ export function Description(props: Readonly<DescriptionProps>) {
   return (
     <HeadlessDescription
       {...props}
-      className="text-primary/60 dark:text-secondary/60 text-sm pb-2"
+      className="text-primary/60 dark:text-secondary/60 text-xs pb-2"
     />
   );
 }

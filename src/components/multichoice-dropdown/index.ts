be88d515e313fab1806c5af414daf22932f212f2
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2025-06-12 21:55:01 +0200

    Delete unused component

--- src/components/multichoice-dropdown/index.ts
+++
@@ -1,6 +0,0 @@
-// SPDX-FileCopyrightText: 2025 Istituto Nazionale di Fisica Nucleare
-//
-// SPDX-License-Identifier: EUPL-1.2
-
-export { MultiChoiceDropdown } from "./multichoice-dropdown";
-export { MultiChoiceItem, type MultiChoiceItemI } from "./multichoice-item";

c538e1b1b0bd041027d5032719bcee1b6a651b91
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2025-07-01 17:21:08 +0200

    Make paginator accessible

--- src/components/paginator/index.tsx
+++ src/components/paginator/index.tsx
@@ -13,15 +13,14 @@ import {
 import Link from "next/link";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
+const className =
+  "flex p-0.5 ml-0 bg-white text-primary/50 border border-primary/10 hover:bg-infn/10 dark:bg-secondary/50 dark:text-secondary dark:hover:text-primary dark:hover:bg-white/70 first:rounded-l-lg last:rounded-r-lg data-[disabled=true]:opacity-30 data-[disabled=true]:pointer-events-none";
+
 export interface PaginatorProps {
   numberOfPages: number;
 }
 
 export default function Paginator(props: Readonly<PaginatorProps>) {
-  const textStyle = "leading-tight text-gray-500 hover:text-gray-700";
-  const buttonStyle =
-    "flex p-0.5 ml-0 bg-white text-primary/50 border border-primary/10 hover:bg-infn/10 dark:bg-secondary/50 dark:text-secondary dark:hover:text-primary dark:hover:bg-white/70";
-
   const { numberOfPages } = props;
   const pathname = usePathname();
   const searchParams = useSearchParams();
@@ -44,18 +43,18 @@ export default function Paginator(props: Readonly<PaginatorProps>) {
   };
 
   return (
-    <div
-      className={`${textStyle} flex w-full items-center justify-between -space-x-px px-4 pb-2`}
-    >
+    <div className="flex items-center justify-between px-4 pb-2">
       <div className="flex items-center space-x-2">
-        <div>
+        <span>
           Page {currentPage} of {numberOfPages}
-        </div>
-        <div>Show</div>
+        </span>
+        <label htmlFor="items-per-page">Show</label>
         <select
+          id="items-per-page"
           value={itemsPerPage}
           className="dark:text-secondary/50 block rounded-lg border border-gray-300 bg-gray-50 p-1 text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-white/10"
           onChange={e => onChangeItemsPerPage(parseInt(e.currentTarget.value))}
+          aria-label="Items per page"
         >
           <option value="10">10</option>
           <option value="20">20</option>
@@ -64,44 +63,40 @@ export default function Paginator(props: Readonly<PaginatorProps>) {
           <option value="200">200</option>
         </select>
       </div>
-      <ul className="flex">
-        <li>
-          <Link
-            title="First Page"
-            className={`${buttonStyle} rounded-l-lg ${currentPage === 1 ? "pointer-events-none opacity-30" : ""}`}
-            href={createPageURL(1)}
-          >
-            <ChevronDoubleLeftIcon className="m-auto w-5" />
-          </Link>
-        </li>
-        <li>
-          <Link
-            title="Previous Page"
-            className={`${buttonStyle} ${currentPage === 1 ? "pointer-events-none opacity-30" : ""}`}
-            href={createPageURL(currentPage - 1)}
-          >
-            <ChevronLeftIcon className="m-auto w-5" />
-          </Link>
-        </li>
-        <li>
-          <Link
-            title="Next Page"
-            className={`${buttonStyle} ${currentPage === numberOfPages ? "pointer-events-none opacity-30" : ""}`}
-            href={createPageURL(currentPage + 1)}
-          >
-            <ChevronRightIcon className="m-auto w-5" />
-          </Link>
-        </li>
-        <li>
-          <Link
-            title="Last Page"
-            className={`${buttonStyle} rounded-r-lg ${currentPage === numberOfPages ? "pointer-events-none opacity-30" : ""}`}
-            href={createPageURL(numberOfPages)}
-          >
-            <ChevronDoubleRightIcon className="m-auto w-5" />
-          </Link>
-        </li>
-      </ul>
+      <div className="flex">
+        <Link
+          title="First Page"
+          className={className}
+          data-disabled={currentPage === 1}
+          href={createPageURL(1)}
+        >
+          <ChevronDoubleLeftIcon className="m-auto w-5" />
+        </Link>
+        <Link
+          title="Previous Page"
+          className={className}
+          data-disabled={currentPage === 1}
+          href={createPageURL(currentPage - 1)}
+        >
+          <ChevronLeftIcon className="m-auto w-5" />
+        </Link>
+        <Link
+          title="Next Page"
+          className={className}
+          data-disabled={currentPage === numberOfPages}
+          href={createPageURL(currentPage + 1)}
+        >
+          <ChevronRightIcon className="m-auto w-5" />
+        </Link>
+        <Link
+          title="Last Page"
+          className={className}
+          data-disabled={currentPage === numberOfPages}
+          href={createPageURL(numberOfPages)}
+        >
+          <ChevronDoubleRightIcon className="m-auto w-5" />
+        </Link>
+      </div>
     </div>
   );
 }

ffb05e8060eaee0b2fe8cf42e3af5f0809fce330
EKACRG <elkihalanouar@gmail.com>
2025-11-25 15:57:09 +0100

    Fetch data for edit policy component

--- src/app/policies/components/scope-policies-form/index.tsx
+++ src/app/policies/components/scope-policies-form/index.tsx
@@ -2,46 +2,74 @@
 //
 // SPDX-License-Identifier: EUPL-1.2
 
-import { Field, Label, Description, Select, SelectOption } from "@/components/form";
+import {
+  Field,
+  Label,
+  Description,
+  Select,
+  SelectOption,
+} from "@/components/form";
 import { Input } from "@/components/inputs";
 import { ScopePolicy } from "@/models/scope-policies";
 
 type ScopePoliciesProps = {
   policy?: ScopePolicy;
-}
+};
 
 const defaultValues = {
   description: "Default Permit ALL policy",
   rule: "VALUE",
   matchingPolicy: "VALUE",
-}
+  account: null,
+  group: null,
+};
 
 const ruleOptions = [
-  {id: "permit", name: "PERMIT"},
-  {id: "deny", name: "DENY"}
+  { id: "permit", name: "PERMIT" },
+  { id: "deny", name: "DENY" },
 ];
 
 const matchingPolicyOptions = [
-  {id: "eq", name: "EQ"},
-  {id: "regexp", name: "REGEXP"},
-  {id: "path", name: "PATH"}
+  { id: "eq", name: "EQ" },
+  { id: "regexp", name: "REGEXP" },
+  { id: "path", name: "PATH" },
+];
+
+const entityOptions = [
+  { id: "null", name: "NULL" },
+  { id: "user", name: "USER" },
+  { id: "group", name: "GROUP" },
 ];
 
 export default function ScopePoliciesForm(props: Readonly<ScopePoliciesProps>) {
   const policy = props.policy ?? defaultValues;
-  const rule = {id: policy.rule, name: policy.rule};
-  const matchingPolicy = {id: policy.matchingPolicy, name: policy.matchingPolicy};
+  const rule = { id: policy.rule, name: policy.rule };
+  const matchingPolicySelector = {
+    id: policy.matchingPolicy,
+    name: policy.matchingPolicy,
+  };
+
+  let entitySelector = { id: "null", name: "NULL" };
+  if (policy.account && !policy.group) {
+    entitySelector = { id: "user", name: "USER" };
+  } else if (!policy.account && policy.group) {
+    entitySelector = { id: "group", name: "GROUP" };
+  } else if (policy.account !== null && policy.group !== null) {
+    throw new Error(
+      "Policy group and account cannot both have value at the same time."
+    );
+  }
 
   return (
     <div className="panel space-y-4">
       <Field>
         <Label>Description</Label>
         <Description>Something users will recognize and trust</Description>
-        <Input 
+        <Input
           type="text"
           name="description"
           title="Description"
-          placeholder={policy.description}
+          placeholder={policy?.description ?? "Default Permit ALL policy"}
           required
         />
       </Field>
@@ -53,7 +81,7 @@ export default function ScopePoliciesForm(props: Readonly<ScopePoliciesProps>) {
           <Select name="rule" defaultValue={rule}>
             {ruleOptions.map(rule => (
               <SelectOption key={rule.id} value={rule}>
-              {rule.name}
+                {rule.name}
               </SelectOption>
             ))}
           </Select>
@@ -61,18 +89,30 @@ export default function ScopePoliciesForm(props: Readonly<ScopePoliciesProps>) {
 
         <Field className="grow">
           <Label>Matching Policy</Label>
-          <Description>Select the right mathing policy</Description>
-          <div className="flex gap-1">
-            <Select name="rule" defaultValue={matchingPolicy}>
-              {matchingPolicyOptions.map(mp => (
+          <Description>Select the right matching policy</Description>
+          <Select name="rule" defaultValue={matchingPolicySelector}>
+            {matchingPolicyOptions.map(mp => (
               <SelectOption key={mp.id} value={mp}>
-                  {mp.name}
+                {mp.name}
               </SelectOption>
-              ))}
-            </Select>
-          </div>
+            ))}
+          </Select>
         </Field>
       </div>
+
+      <Field>
+        <Label>Account/Group</Label>
+        <Description>Select account or group</Description>
+        <div className="flex gap-2">
+          <Select name="rule" defaultValue={entitySelector}>
+            {entityOptions.map(ep => (
+              <SelectOption key={ep.id} value={ep}>
+                {ep.name}
+              </SelectOption>
+            ))}
+          </Select>
+        </div>
+      </Field>
     </div>
   );
 }

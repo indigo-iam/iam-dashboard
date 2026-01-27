6d9721e845995f3ae12af63055a3cb07dc04e811
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2026-01-27 17:39:31 +0100

    Make better-auth support redirect_uris with different basePaths

--- src/app/api/auth/[...all]/route.ts
+++ src/app/api/auth/[...all]/route.ts
@@ -2,7 +2,32 @@
 //
 // SPDX-License-Identifier: EUPL-1.2
 
-import { auth } from "@/auth/server";
+import { auth } from "@/auth";
 import { toNextJsHandler } from "better-auth/next-js";
+import { NextRequest } from "next/server";
+import { settings } from "@/config";
 
-export const { POST, GET } = toNextJsHandler(auth);
+const { IAM_DASHBOARD_URL } = settings;
+
+const handlers = toNextJsHandler(auth);
+const baseURL = IAM_DASHBOARD_URL;
+
+// Workaround to support different basePath
+// https://github.com/better-auth/better-auth/issues/4715#issuecomment-3439681277
+
+function rewriteRequest(req: NextRequest) {
+  const { search, pathname } = req.nextUrl;
+  const url = new URL(`${baseURL}${pathname}`);
+  url.search = search;
+  return new NextRequest(url, req);
+}
+
+export async function GET(req: NextRequest) {
+  const modified = rewriteRequest(req);
+  return handlers.GET(modified);
+}
+
+export async function POST(req: NextRequest) {
+  const modified = rewriteRequest(req);
+  return handlers.POST(modified);
+}

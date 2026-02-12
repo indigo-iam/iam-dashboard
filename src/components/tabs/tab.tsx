b159efa1c0e36e640d2e73f6d4783b341772ed82
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2026-02-12 11:25:59 +0100

    Minor refactor of tailwind classes

--- src/components/tabs/tab.tsx
+++ src/components/tabs/tab.tsx
@@ -11,7 +11,7 @@ export type TabProps = {
 export function Tab(props: Readonly<TabProps>) {
   const { children } = props;
   return (
-    <HeadlessUITab className="dark:text-light-gray/80 text-gray min-w-fit grow cursor-pointer overflow-hidden border-b-2 border-gray-400 p-2 text-sm tracking-wide -outline-offset-2 hover:border-sky-500 hover:text-sky-500 focus:not-data-focus:outline-none data-[selected]:border-sky-600 data-[selected]:font-bold data-[selected]:text-sky-500 dark:hover:border-blue-300 dark:hover:text-blue-300 dark:data-[selected]:border-sky-400 dark:data-[selected]:text-sky-500">
+    <HeadlessUITab className="dark:text-light-gray/80 text-gray min-w-fit grow cursor-pointer overflow-hidden border-b-2 border-gray-400 p-2 text-sm tracking-wide -outline-offset-2 hover:border-sky-500 hover:text-sky-500 focus:not-data-focus:outline-none data-selected:border-sky-600 data-selected:font-bold data-selected:text-sky-500 dark:hover:border-blue-300 dark:hover:text-blue-300 dark:data-selected:border-sky-400 dark:data-selected:text-sky-500">
       {children}
     </HeadlessUITab>
   );

e11372fd5f56d44eaa0f0253d71bc281e2e6ec22
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2026-02-12 11:25:35 +0100

    Avoid to call setState synchronously inside useEffect

--- src/components/tabs/tab-group.tsx
+++ src/components/tabs/tab-group.tsx
@@ -4,24 +4,26 @@
 
 "use client";
 
+import { useSessionStorage } from "@/utils/hooks";
 import { TabGroup as HeadlessTabList, TabGroupProps } from "@headlessui/react";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export function TabGroup(props: Readonly<TabGroupProps>) {
   const pathname = usePathname();
-
+  const { setItem, getItem } = useSessionStorage();
   const [index, setIndex] = useState(0);
 
   useEffect(() => {
-    const tabIndex = sessionStorage.getItem(`${pathname}?tabIndex`);
-    if (tabIndex) {
-      setIndex(parseInt(tabIndex));
+    async function getIndex() {
+      const tabIndex = parseInt(getItem(`${pathname}?tabIndex`) ?? "0");
+      setIndex(tabIndex);
     }
-  }, [pathname]);
+    getIndex();
+  }, [getItem, pathname]);
 
   function onTabChange(index: number) {
-    sessionStorage.setItem(`${pathname}?tabIndex`, index.toString());
+    setItem(`${pathname}?tabIndex`, index.toString());
     setIndex(index);
   }
 
---
+++ src/utils/hooks/index.tsx
@@ -0,0 +1,25 @@
+// SPDX-FileCopyrightText: 2025 Istituto Nazionale di Fisica Nucleare
+//
+// SPDX-License-Identifier: EUPL-1.2
+
+import { useState, useEffect } from "react";
+
+export function useSessionStorage() {
+  const [storage, setStorage] = useState<Storage>();
+
+  useEffect(() => {
+    (async () => {
+      setStorage(window.sessionStorage);
+    })();
+  }, []);
+
+  function getItem(key: string) {
+    return storage?.getItem(key);
+  }
+
+  function setItem(key: string, value: string) {
+    storage?.setItem(key, value);
+  }
+
+  return { getItem, setItem };
+}

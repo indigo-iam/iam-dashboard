ebf9f1f187b675e793310cd7f2115d242a1ae40c
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2026-02-17 18:54:03 +0100

    Polish simple test

--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -3,6 +3,6 @@ import { test, expect } from "./auth.fixture";
 test("Login w/IAM", async ({ page }) => {
   await test.step("login", async () => {
     await page.goto("./");
-    expect(await page.getByLabel("Username").inputValue()).toBe("admin");
+    await expect(page.getByLabel("Username")).toHaveValue("admin");
   });
 });

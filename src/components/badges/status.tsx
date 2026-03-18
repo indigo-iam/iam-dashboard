8d0da130a2ef3fc2de917f38940fd6317e347362
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2026-03-18 15:19:03 +0100

    Autohide satus

--- src/components/badges/status.tsx
+++ src/components/badges/status.tsx
@@ -4,19 +4,38 @@
 
 type StatusProps = {
   active: boolean;
+  autoHide?: boolean;
 };
 
 export default function Status(props: Readonly<StatusProps>) {
-  const { active } = props;
+  const { active, autoHide } = props;
   const title = `${active ? "Active" : "Disabled"}`;
   return (
     <div
       title={title}
-      className="text-danger data-[enabled=true]:text-success dark:bg-danger dark:data-[enabled=true]:bg-success rounded-full bg-red-100 px-2 py-0.5 text-xs data-[enabled=true]:bg-green-100 dark:text-white dark:data-[enabled=true]:text-white"
+      className="data-[autohide=true]:bg-danger lg:data-[autohide=true]:text-danger data-[enabled=true]:data-[autohide=true]:bg-success lg:data-[autohide=true]:data-[enabled=true]:text-success text-danger data-[enabled=true]:text-success dark:bg-danger dark:data-[enabled=true]:bg-success dark:lg:data-[autohide=true]:data-[enabled=true]:bg-success dark:lg:data-[autohide=true]:data-[enabled=false]:bg-danger min-w-fit rounded-full bg-red-100 text-xs 
+      
+      px-2 py-0.5
+      
+      data-[autohide=true]:p-0 
+      
+  
+     
+     
+     
+      data-[autohide=true]:h-3 data-[autohide=true]:w-3 data-[enabled=true]:bg-green-100 lg:data-[autohide=true]:h-auto lg:data-[autohide=true]:min-w-fit lg:data-[autohide=true]:bg-red-100 lg:data-[autohide=true]:px-2 lg:data-[autohide=true]:py-0.5 lg:data-[enabled=true]:data-[autohide=true]:bg-green-100 dark:text-white dark:data-[enabled=true]:text-white dark:lg:data-[autohide=true]:data-[enabled=false]:text-white dark:lg:data-[autohide=true]:data-[enabled=true]:text-white"
+      
+      
       data-enabled={active}
+      data-autohide={autoHide}
       aria-label={title}
     >
-      <span className="inline-block">{title}</span>
+      <span
+        className="inline-block data-[autohide=true]:hidden lg:data-[autohide=true]:inline-block"
+        data-autohide={autoHide}
+      >
+        {title}
+      </span>
     </div>
   );
 }

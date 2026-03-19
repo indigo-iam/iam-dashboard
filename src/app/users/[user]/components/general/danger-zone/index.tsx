0de4fb9d863017fc5b05e9e47509d4a0254fd2c6
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2026-03-19 10:01:33 +0100

    Adjust user danger zone layout

--- src/app/users/[user]/components/general/danger-zone/index.tsx
+++ src/app/users/[user]/components/general/danger-zone/index.tsx
@@ -16,14 +16,16 @@ export function DangerZone(props: Readonly<DangerZoneProps>) {
   const { user } = props;
   return (
     <div className="flex flex-col gap-8 py-4 lg:flex-row">
-      <div className="flex w-full flex-col text-sm font-light lg:w-1/3">
-        <span className="text-danger dark:text-danger-light font-semibold">
+      <div className="flex w-full flex-col space-y-2 text-sm font-light lg:w-1/3">
+        <h5 className="text-danger dark:text-danger-light font-semibold">
           Danger zone
-        </span>
-        <p>
-          A disabled user cannot login and their Access Tokens and Refresh
-          Tokens are immediately revoked.
-        </p>
+        </h5>
+        <div className="space-y-1">
+          <p className="whitespace-normal">
+            A disabled user cannot login and tokens are immediately revoked.
+          </p>
+          <p>Delete the user to completely remove them from he organization.</p>
+        </div>
       </div>
       <div className="w-full space-y-4 lg:w-2/3">
         <div className="flex flex-wrap gap-4">

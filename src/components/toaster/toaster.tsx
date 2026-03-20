e66d25a823611f4722838cc5ac2d806bac5ca573
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2026-03-20 09:52:39 +0100

    Dynamicall position toaster for mobile

--- src/components/toaster/toaster.tsx
+++ src/components/toaster/toaster.tsx
@@ -4,7 +4,7 @@
 
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Toaster, toast } from "sonner";
 import {
   CheckCircleIcon,
@@ -15,10 +15,50 @@ import {
 } from "@heroicons/react/24/outline";
 import { Notification, NotificationType } from "./types";
 
+const breakpoints = {
+  sm: 640,
+  md: 768,
+  lg: 1024,
+  xl: 1280,
+};
+
+function useUpdateToasterPosition() {
+  const [position, setPosition] = useState<"top-right" | "bottom-right">(
+    "bottom-right"
+  );
+
+  function resize() {
+    const width = window.innerWidth;
+    if (width < breakpoints.sm) {
+      setPosition("bottom-right");
+    } else {
+      setPosition("top-right");
+    }
+  }
+
+  useEffect(() => {
+    let timeoutId: ReturnType<typeof setTimeout>;
+
+    const handleResize = () => {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(resize, 150);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      clearTimeout(timeoutId);
+    };
+  });
+  return position;
+}
+
 export default function MyToaster(
   props: Readonly<{ notification?: Notification }>
 ) {
   const { notification } = props;
+  const position = useUpdateToasterPosition();
 
   useEffect(() => {
     if (!notification) {
@@ -33,10 +73,14 @@ export default function MyToaster(
       />
     ));
   }, [notification]);
-  return <Toaster position="top-right" />;
+  return <Toaster position={position} />;
 }
 
-let Icon = (props: Readonly<{ type: NotificationType }>) => {
+type IconProps = {
+  type: NotificationType;
+};
+
+function Icon(props: Readonly<IconProps>) {
   const { type } = props;
   switch (type) {
     case "info":
@@ -48,53 +92,61 @@ let Icon = (props: Readonly<{ type: NotificationType }>) => {
       );
     case "success":
       return (
-        <CheckCircleIcon key="check-icon" className="text-success size-6" />
+        <CheckCircleIcon
+          key="check-icon"
+          className="text-success size-6 flex-none"
+        />
       );
     case "warning":
       return (
         <ExclamationTriangleIcon
           key="danger-icon"
-          className="text-warning size-6"
+          className="text-warning size-6 flex-none"
         />
       );
     case "error":
       return (
         <ExclamationCircleIcon
           key="danger-circle-icon"
-          className="text-danger size-6"
+          className="text-danger size-6 flex-none"
         />
       );
   }
-};
+}
 
-const CustomToast = (props: {
+type CustomToastProps = {
   title: string;
   subtitle?: string;
   dismiss: () => void;
   type: NotificationType;
-}) => {
+};
+
+function CustomToast(props: Readonly<CustomToastProps>) {
   const { title, subtitle, dismiss, type } = props;
   return (
-    <div className="w-80" data-testid="toast">
-      <div className="overlay flex w-full items-center border p-3">
-        <div className="flex grow items-center gap-2">
-          <Icon type={type} />
-          <div>
-            <p className="text-normal font-semibold">{title}</p>
-            <p className="block text-sm text-gray-500">{subtitle}</p>
-          </div>
+    <div
+      className="overlay flex w-full items-center border px-3 py-2"
+      data-testid="toast"
+    >
+      <div className="flex grow items-center gap-2">
+        <Icon type={type} />
+        <div>
+          <p className="text-normal font-semibold">{title}</p>
+          <p className="block text-sm text-gray-500 dark:text-gray-300">
+            {subtitle}
+          </p>
         </div>
-        <button
-          title="Close"
-          className="w-8 cursor-pointer rounded-full p-1.25 text-gray-500 hover:bg-gray-300 dark:text-gray-200 dark:hover:bg-gray-500"
-          onClick={dismiss}
-        >
-          <XCircleIcon />
-        </button>
       </div>
+      <button
+        title="Close"
+        className="w-8 flex-none cursor-pointer rounded-full p-1.25 text-gray-500 hover:bg-gray-300 dark:text-gray-200 dark:hover:bg-gray-500"
+        onClick={dismiss}
+      >
+        <XCircleIcon />
+      </button>
     </div>
   );
-};
+}
 
 export const toaster = {
   info: (message: string, subtitle?: string) =>

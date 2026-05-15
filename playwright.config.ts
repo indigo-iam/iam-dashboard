0c1251c5bdfeb54a1ade22ec6f6d55c4f45eb031
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2026-05-15 10:49:23 +0200

    Rename tests projects

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,15 +39,15 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: "chromium",
+      name: "Desktop Chrome",
       use: { ...devices["Desktop Chrome"] },
     },
     {
-      name: "firefox",
+      name: "Desktop Firefox",
       use: { ...devices["Desktop Firefox"] },
     },
     {
-      name: "webkit",
+      name: "Desktop Safari",
       use: { ...devices["Desktop Safari"] },
     },
     /* Test against mobile viewports. */

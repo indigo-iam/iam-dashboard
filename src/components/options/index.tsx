2cff31b81b56e0c1b8edecd4cf733400a74bba5c
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2026-06-08 16:41:54 +0200

    Convert Options from Menu to Popover component

--- src/components/options/index.tsx
+++ src/components/options/index.tsx
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: EUPL-1.2
 
-import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/react";
+import { Popover, PopoverButton, PopoverPanel } from "@headlessui/react";
 import { EllipsisHorizontalIcon } from "@heroicons/react/24/solid";
 
 type OptionProps = {
@@ -15,14 +15,12 @@ type OptionProps = {
 export function Option(props: Readonly<OptionProps>) {
   const { children, ...other } = props;
   return (
-    <MenuItem>
-      <button
-        className="hover:not:dark:text-gray-500 data-danger:text-danger dark:data-danger:text-danger-light btn-popover px-2 py-1.5 text-start text-base whitespace-nowrap focus:outline-none dark:text-gray-200"
-        {...other}
-      >
-        {children}
-      </button>
-    </MenuItem>
+    <button
+      className="hover:not:dark:text-gray-500 data-danger:text-danger dark:data-danger:text-danger-light btn-popover px-2 py-1.5 text-start text-base whitespace-nowrap dark:text-gray-200"
+      {...other}
+    >
+      {children}
+    </button>
   );
 }
 
@@ -33,21 +31,21 @@ type OptionsProps = {
 export function Options(props: Readonly<OptionsProps>) {
   const { children } = props;
   return (
-    <Menu>
-      <MenuButton
+    <Popover>
+      <PopoverButton
         data-testid="option"
         title="More"
-        className="my-auto cursor-pointer rounded-md transition hover:bg-gray-200 focus:outline-none data-open:bg-gray-200 dark:hover:bg-gray-500 dark:data-active:bg-gray-200 dark:data-open:bg-gray-500"
+        className="my-auto cursor-pointer rounded-md transition hover:bg-gray-200 data-open:bg-gray-200 dark:hover:bg-gray-500 dark:data-active:bg-gray-200 dark:data-open:bg-gray-500"
       >
         <EllipsisHorizontalIcon className="size-8 text-gray-800 dark:text-gray-400" />
-      </MenuButton>
-      <MenuItems
+      </PopoverButton>
+      <PopoverPanel
         anchor="bottom"
         transition
         className="easy-out overlay flex flex-col p-2 data-closed:opacity-0"
       >
         {children}
-      </MenuItems>
-    </Menu>
+      </PopoverPanel>
+    </Popover>
   );
 }

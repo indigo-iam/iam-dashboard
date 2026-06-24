cd076a4538910d3389adbf0156c5b4000ab7254a
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2026-06-24 15:59:01 +0200

    Remove unused import

--- src/app/users/[user]/components/groups/managed/index.tsx
+++ src/app/users/[user]/components/groups/managed/index.tsx
@@ -2,7 +2,6 @@
 //
 // SPDX-License-Identifier: EUPL-1.2
 
-import { User } from "@/models/scim";
 import { fetchManagedGroups } from "@/services/groups";
 import { ManagedGroup } from "@/models/groups";
 import Link from "@/components/link";
@@ -62,5 +61,5 @@ export default async function ManagedGroups(
     <Suspense>
       <Content userId={userId} />
     </Suspense>
-  )
+  );
 }

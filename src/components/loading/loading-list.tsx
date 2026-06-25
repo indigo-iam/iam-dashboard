38e95019c23d4a102186463ca6f87ac2154374c4
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2026-06-25 10:35:08 +0200

    Adjust LoadingList style

--- src/components/loading/loading-list.tsx
+++ src/components/loading/loading-list.tsx
@@ -4,7 +4,7 @@
 
 function PlaceholderRow() {
   return (
-    <li className="iam-list-item px-2 py-6" data-disabled={true}>
+    <li className="lex flex-row items-center rounded-l-sm border-b border-l-4 border-l-transparent px-2 py-6 text-gray-500 last:border-transparent dark:text-gray-400">
       <div className="w-full animate-pulse space-y-4">
         <div className="h-2.5 w-4/5 rounded bg-gray-200 dark:bg-gray-500" />
         <div className="h-2.5 w-1/3 rounded bg-gray-200 dark:bg-gray-500" />

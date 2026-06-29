24951826450bc92e54518c68e4ebf5ada3cbcf7c
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2026-06-29 12:23:02 +0200

    Move groups requests to the bottom

--- src/app/groups/(overview)/page.tsx
+++ src/app/groups/(overview)/page.tsx
@@ -93,12 +93,6 @@ async function UserPage() {
         />
       </header>
       <div className="container space-y-4">
-        {requests.length > 0 ? (
-          <div className="panel">
-            <h3 className="py-2">Sent requests</h3>
-            <GroupRequestsTable userId={me.id} requests={requests} />
-          </div>
-        ) : null}
         <div className="panel">
           <h3 className="py-2">Groups</h3>
           {groups.length > 0 ? (
@@ -111,6 +105,12 @@ async function UserPage() {
           <div className="panel">
             <h3 className="py-2">Managed groups</h3>
             <UserManagedGroupsTable groups={managedGroups} />
+          </div>
+        )}
+        {requests.length > 0 && (
+          <div className="panel">
+            <h3 className="py-2">Pending requests</h3>
+            <GroupRequestsTable userId={me.id} requests={requests} />
           </div>
         )}
       </div>

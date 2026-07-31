26217fa7d7248d2d6ae40fafeca7c1724687c4e8
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2026-07-31 20:54:07 +0200

    Adjust LoadingForm placeholder dark mode color

--- src/components/loading/loading-form.tsx
+++ src/components/loading/loading-form.tsx
@@ -4,10 +4,10 @@
 
 function FormItem() {
   return (
-    <div className="animate-pulse space-y-2">
-      <div className="h-3 w-2/6 rounded-full bg-neutral-100" />
-      <div className="h-8 w-full rounded bg-neutral-100" />
-      <div className="h-2 w-1/6 rounded-full bg-neutral-100" />
+    <div className="space-y-2">
+      <div className="h-3 w-2/6 rounded-full bg-neutral-100 dark:bg-gray-700" />
+      <div className="h-8 w-full rounded bg-neutral-100 dark:bg-gray-700" />
+      <div className="h-2 w-1/6 rounded-full bg-neutral-100 dark:bg-gray-700" />
     </div>
   );
 }
@@ -15,10 +15,10 @@ function FormItem() {
 export function LoadingForm() {
   return (
     <div className="panel">
-      <div className="flex flex-col gap-8 pb-4 lg:flex-row">
+      <div className="flex animate-pulse flex-col gap-8 pb-4 lg:flex-row">
         <div className="w-full space-y-2 text-sm font-light lg:w-1/3">
-          <div className="h-3 w-full rounded-full bg-neutral-100" />
-          <div className="h-12 w-full rounded bg-neutral-100" />
+          <div className="h-3 w-full rounded-full bg-neutral-100 dark:bg-gray-700" />
+          <div className="h-12 w-full rounded bg-neutral-100 dark:bg-gray-700" />
         </div>
         <div className="w-full space-y-4 lg:w-2/3">
           <FormItem />

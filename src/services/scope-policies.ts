6106e3c5ceb296afb2426bb87e26fbe7b6405059
EKACRG <elkihalanouar@gmail.com>
2026-08-12 11:40:02 +0200

    Add scope policy create and update methods

--- src/services/scope-policies.ts
+++ src/services/scope-policies.ts
@@ -4,9 +4,10 @@
 
 "use server";
 
-import { getItem } from "@/utils/fetch";
-import { ScopePolicy } from "@/models/scope-policies";
+import { authFetch, getItem } from "@/utils/fetch";
+import { ScopePolicy, ScopePolicyRequest } from "@/models/scope-policies";
 import { settings } from "@/config";
+import { revalidatePath } from "next/cache";
 
 const { IAM_API_URL } = settings;
 
@@ -18,4 +19,37 @@ export async function fetchScopePolicies() {
 export async function fetchScopePolicy(id: number) {
   const url = `${IAM_API_URL}/iam/scope_policies/${id}`;
   return await getItem<ScopePolicy>(url);
+}
+
+export async function addScopePolicy(policy: ScopePolicyRequest) {
+  const url = `${IAM_API_URL}/iam/scope_policies`;
+  const response = await authFetch(url, {
+    body: JSON.stringify(policy), method: "POST", headers: {
+      "content-type": "application/json"
+    }
+  });
+  if (response.ok) {
+    console.log(await response.text());
+    revalidatePath("/policies");
+  }
+  else {
+    console.log(`${response.status} ${await response.text()}`);
+  }
+}
+
+export async function updateScopePolicy(id: number, policy: ScopePolicyRequest) {
+  const url = `${IAM_API_URL}/iam/scope_policies/${id}`;
+  const response = await authFetch(url, {
+    body: JSON.stringify(policy), method: "PUT", headers: {
+      "content-type": "application/json"
+    }
+  });
+  if (response.ok) {
+    console.log(await response.text());
+    revalidatePath(`/policies/${id}`);
+    revalidatePath("/policies");
+  }
+  else {
+    console.log(`${response.status} ${await response.text()}`);
+  }
 }
